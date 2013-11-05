32382815046c1a576a9539e0879a88552f66a9c2
Abimael Medina <webstudent@PC16296.local>
2013-11-04 18:06:56 -0600

    My version

---
+++ Trailer1ViewController.h
@@ -0,0 +1,18 @@
+//
+//  Trailer1ViewController.h
+//  MyMovies
+//
+//  Created by webstudent on 11/4/13.
+//  Copyright (c) 2013 Abimael Medina. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <MediaPlayer/MediaPlayer.h>
+
+@interface Trailer1ViewController : UIViewController
+
+{
+    MPMoviePlayerController *player;
+}
+
+@end
---
+++ Trailer2ViewController.h
@@ -0,0 +1,18 @@
+//
+//  Trailer2ViewController.h
+//  MyMovies
+//
+//  Created by webstudent on 11/4/13.
+//  Copyright (c) 2013 Abimael Medina. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <MediaPlayer/MediaPlayer.h>
+
+@interface Trailer2ViewController : UIViewController
+
+{
+    MPMoviePlayerController *player;
+}
+
+@end
